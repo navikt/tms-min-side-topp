99461862c904ddb3e42e8c0733eb6b455129e286
sasoria <sergio.alberto.arevalo.soria@nav.no>
2022-03-21 13:35:49 +0100

    Tester et kall mot min-side-proxy

--- src/api/urls.js
+++ src/api/urls.js
@@ -2,8 +2,8 @@ import { getEnvironment } from "../utils/environment";
 
 const NAVN_URL = {
   local: "https://www.dev.nav.no/person/dittnav-api/navn",
-  development: "https://person.dev.nav.no/tms-min-side-proxy/dittnav/navn",
-  production: "https://person.intern.nav.no/person/dittnav-api/navn",
+  development: "https://person.dev.nav.no/tms-min-side-proxy/dittnav/test",
+  production: "https://person.intern.nav.no/person/dittnav-api/test",
 };
 
 export const navnUrl = NAVN_URL[getEnvironment()];
