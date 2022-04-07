7fb7c4a9e1a64e2124006f6d4803e104a0b8510c
sasoria <sergio.alberto.arevalo.soria@nav.no>
2022-04-07 11:15:56 +0200

    Retter opp en skrivefeil

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./App.less";
 const App = (props) => {
   console.log(props);
 
-  props.emitter.emitter("loaded", "loaded successfully");
+  props.emitter("loaded", "loaded successfully");
   props.emitter.on("level", (level) => console.log(level));
 
   return (
